4a25134f40c11d9f70e86a38a543227626727894
FathimaHusna <husnassf.19@uom.lk>
2025-01-07 16:51:02 +0530

    cleaned code

--- pizza-billing-backend/database/db.go
+++ pizza-billing-backend/database/db.go
@@ -1,23 +1,3 @@
-// package database
-
-// import (
-// 	"log"
-
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// func ConnectDB() {
-// 	dsn := "host=localhost user=postgres password=1234 dbname=pizzshop port=5432 sslmode=disable"
-// 	var err error
-// 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatal("Failed to connect to database")
-// 	}
-// 	log.Println("Database Connected successfully!")
-// }
 
 package database
 
